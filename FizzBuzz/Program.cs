5d7d828683ba94d9904d091dd5b10cc80cb73932
Erick Nave <enave@synergydatasystems.com>
2013-07-03 12:29:47 -0400

    added code

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+using System.Diagnostics;
+
+namespace FizzBuzz
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (var i = 1; i <= 100; i++)
+			{
+				if (i%3 == 0)
+				{
+					Debug.Write("Fizz");
+				}
+
+				if (i%5 == 0)
+				{
+					Debug.Write("Buzz");
+				}
+
+				if (i%3 != 0 && i%5 != 0)
+				{
+					Debug.Write(i);
+				}
+
+				Debug.WriteLine("");
+			}
+
+			//Console.WriteLine("Hit Enter to Quit.");
+			//Console.ReadLine();
+		}
+	}
+}
